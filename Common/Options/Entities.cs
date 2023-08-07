cb9d774d6c4676824413e815011e4412ac675917
Mike Farr <mike@tokenize.ca>
2023-08-07 08:12:14 -0400

    Implement support for basic configuration (#1)

---
+++ Common/Options/Entities.cs
@@ -0,0 +1,26 @@
+﻿namespace Common.Options;
+
+public enum LoggingLevel
+{
+    Error,
+    Debug,
+    Trace
+}
+
+public sealed class LoggingOptions
+{
+    public required string Name { get; set; }
+
+    public required string Type { get; set; }
+
+    public required LoggingLevel Level { get; set; }
+}
+
+public sealed class ConnectionOptions
+{
+    public required string Ip { get; set; }
+
+    public required int Port { get; set; }
+
+    public required string Password { get; set; }
+}
