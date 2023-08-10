2b5b9abd06f374077c459b040e5df6aabf76ad18
Mike Farr <mike@tokenize.ca>
2023-08-09 20:01:50 -0400

    Add boilerplate for NetworkService

--- CommandLine/Program.cs
+++ CommandLine/Program.cs
@@ -5,6 +5,7 @@ using Common.Options;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Network;
 
 #endregion
 
@@ -22,6 +23,7 @@ builder.Services.AddOptions<LoggingOptions>()
     .Bind(builder.Configuration.GetSection("Logging"));
 
 builder.Services.AddHostedService<ConsoleHost>();
+builder.Services.AddHostedService<NetworkService>();
 
 builder.Services.AddLogging();
 
---
+++ Network/NetworkService.cs
@@ -0,0 +1,32 @@
+﻿using Common.Options;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Network;
+
+public class NetworkService : IHostedService
+{
+    private readonly IOptions<ConnectionOptions> _options;
+
+    private readonly ILogger<NetworkService> _logger;
+    
+    public NetworkService(IOptions<ConnectionOptions> options, ILogger<NetworkService> logger)
+    {
+        _options = options;
+
+        _logger = logger;
+    }
+    
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogDebug("Starting network service");
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
