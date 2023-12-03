00bc57fbcf60d8da066d9b432210918f01f8dddd
Mike Farr <mike@noneleft.ca>
2023-12-03 06:52:31 -0500

    Move ConfigurationException to Exceptions namespace

--- Common/Exceptions/ConfigurationException.cs
+++ Common/Exceptions/ConfigurationException.cs
@@ -1,6 +1,6 @@
 ﻿using System.Runtime.Serialization;
 
-namespace Common.Options;
+namespace Common.Exceptions;
 
 public class ConfigurationException : Exception
 {
--- Common/Exceptions/DeviceDisconnectException.cs
+++ Common/Exceptions/DeviceDisconnectException.cs
@@ -1,10 +1,22 @@
+using System.Runtime.Serialization;
+
 namespace Common.Exceptions;
 
 public class DeviceDisconnectException : Exception
 {
-    public DeviceDisconnectException() { }
-    
-    public DeviceDisconnectException(string message) : base(message) { }
-    
-    public DeviceDisconnectException(string message, Exception? innerException) : base(message, innerException) { }
+    public DeviceDisconnectException()
+    {
+    }
+
+    protected DeviceDisconnectException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
+    public DeviceDisconnectException(string? message) : base(message)
+    {
+    }
+
+    public DeviceDisconnectException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
 }
